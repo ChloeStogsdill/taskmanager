bb7561acae0d5e23af3e8eb90b20b56a1c398ae8
ChloeStogsdill <peaceloveandsoccer25@gmail.com>
2024-05-12 18:01:34 -0400

    coded view enum

---
+++ src/main/java/View.java
@@ -0,0 +1,13 @@
+public enum View {
+    LOGIN ("login-page.fxml");
+
+    private String filename;
+
+    View(String filename) {
+        this.filename = filename;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+}
