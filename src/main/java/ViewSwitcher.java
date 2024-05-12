98b51dac11f9965559b54e0a4e0192c4a78fb476
ChloeStogsdill <peaceloveandsoccer25@gmail.com>
2024-05-12 18:00:04 -0400

    coded view switcher

--- src/main/java/ViewSwitcher.java
+++ src/main/java/ViewSwitcher.java
@@ -1,2 +1,26 @@
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class ViewSwitcher {
+    public static Scene scene;
+
+    public static void setScene(Scene scene) {
+        ViewSwitcher.scene = scene;
+    }
+
+    public static void switchTo(View view){
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(Objects.requireNonNull(ViewSwitcher.class.getResource(view.getFilename())));
+            Parent root = fxmlLoader.load();
+
+            Object controller = fxmlLoader.getController();
+            scene.setRoot(root);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
