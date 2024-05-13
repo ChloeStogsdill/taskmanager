5f134536f83a513864515fd555637af65660c249
ChloeStogsdill <peaceloveandsoccer25@gmail.com>
2024-05-12 21:21:59 -0400

    made task class getters setters and constructors

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -28,4 +28,78 @@ public class Task {
 
     @Column(name = "RecurrencePeriod")
     private String RecurrencePeriod;
+
+    public Task(){}
+
+    public Task(String username, String title, String description, String dueDate, String className, String recurring, String recurrencePeriod) {
+        this.Username = username;
+        this.Title = title;
+        this.Description = description;
+        this.Recurring = Boolean.parseBoolean(recurring);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getDueDate() {
+        return DueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        DueDate = dueDate;
+    }
+
+    @Override
+    public String getClass() {
+        return Class;
+    }
+
+    public void setClass(String aClass) {
+        Class = aClass;
+    }
+
+    public boolean isRecurring() {
+        return Recurring;
+    }
+
+    public void setRecurring(boolean recurring) {
+        Recurring = recurring;
+    }
+
+    public String getRecurrencePeriod() {
+        return RecurrencePeriod;
+    }
+
+    public void setRecurrencePeriod(String recurrencePeriod) {
+        RecurrencePeriod = recurrencePeriod;
+    }
 }
