47c16c5659848bce54ab97c618811c3b27e373a9
ChloeStogsdill <peaceloveandsoccer25@gmail.com>
2024-05-12 21:14:59 -0400

    made task manager extend application to run at start

--- src/main/java/TaskManagerApplication.java
+++ src/main/java/TaskManagerApplication.java
@@ -1,10 +1,11 @@
+import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import static javafx.application.Application.launch;
 
-public class TaskManagerApplication {
+public class TaskManagerApplication extends Application {
     public static void main(String[] args) {launch(args);}
 
     public void start(Stage stage){
