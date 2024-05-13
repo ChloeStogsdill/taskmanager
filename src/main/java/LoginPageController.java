a6dd7bf6f5549d307e8a3205e1f6716e88a58e7f
ChloeStogsdill <peaceloveandsoccer25@gmail.com>
2024-05-12 21:07:14 -0400

    added login controller framework

--- src/main/java/LoginPageController.java
+++ src/main/java/LoginPageController.java
@@ -1,2 +1,32 @@
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import org.hibernate.Session;
+
 public class LoginPageController {
+    private static Session session;
+
+    public void loginButtonClicked(ActionEvent event) {
+
+    }
+
+    public void registerButtonClicked(ActionEvent event) {
+
+    }
+
+    private void addUser(String username, String password){
+
+    }
+
+    private boolean userExists(String username){
+        return false;
+    }
+
+    public void switchToTasks(ActionEvent event) {
+
+    }
+
+    public void exitButtonClicker(ActionEvent event) {
+        Platform.exit();
+    }
+
 }
