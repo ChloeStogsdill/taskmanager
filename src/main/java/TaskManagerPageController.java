65c0039f9078219484fc564f978bc9e2bcf97dc0
ChloeStogsdill <peaceloveandsoccer25@gmail.com>
2024-05-13 22:12:44 -0400

    connected fxml to controller

--- src/main/java/TaskManagerPageController.java
+++ src/main/java/TaskManagerPageController.java
@@ -1,2 +1,85 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import org.hibernate.Session;
+
 public class TaskManagerPageController {
+    private static Session session;
+
+    @FXML
+    TableView<Task> allTasksTable;
+
+    @FXML
+    TableColumn<Task, String> taskNameColumn;
+
+    @FXML
+    TableColumn<Task, String> taskDescriptionColumn;
+
+    @FXML
+    TableColumn<Task, String> taskClassColumn;
+
+    @FXML
+    TableColumn<Task, String> taskDueDateColumn;
+
+    @FXML
+    TableColumn<Task, Integer> taskPriorityColumn;
+
+    @FXML
+    TableView<Task> allClassesTable;
+
+    @FXML
+    TableColumn<ClassFor, String> classNameColumn;
+
+    @FXML
+    TableColumn<ClassFor, String> classCRNColumn;
+
+    @FXML
+    TableColumn<ClassFor, String> classProfessorColumn;
+
+    @FXML
+    TableColumn<ClassFor, String> classDatesColumn;
+
+    @FXML
+    TableColumn<ClassFor, Integer> classTimeColumn;
+
+    @FXML
+    TextField taskTitleField;
+
+    @FXML
+    TextArea descriptionField;
+
+    @FXML
+    SplitMenuButton classSelector;
+
+    @FXML
+    TextField priorityField;
+
+    @FXML
+    TextField classField;
+
+    @FXML
+    TextField crnField;
+
+    @FXML
+    TextField professorField;
+
+    @FXML
+    TextField meetingDaysField;
+
+    @FXML
+    TextField meetingTimesField;
+
+    public void addTaskClicked(ActionEvent event){
+
+    }
+
+    public  void addClassClicked(ActionEvent event){
+
+    }
+
+    public void signOutButtonClicked(ActionEvent event){
+        ViewSwitcher.switchTo(View.LOGIN);
+    }
+
+
 }
