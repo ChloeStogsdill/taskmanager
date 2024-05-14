39bc88bf949274f421489ecf5d893ca17f2e056e
ChloeStogsdill <peaceloveandsoccer25@gmail.com>
2024-05-14 14:11:59 -0400

    added populate class section method for menu dropdown

--- src/main/java/TaskManagerPageController.java
+++ src/main/java/TaskManagerPageController.java
@@ -2,6 +2,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
+import java.util.List;
 
 public class TaskManagerPageController {
     private static Session session;
@@ -89,6 +91,26 @@ public class TaskManagerPageController {
 
     public void signOutButtonClicked(ActionEvent event){
         ViewSwitcher.switchTo(View.LOGIN);
+    }
+
+    private void populateClassSelection(){
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            Query<ClassFor> query = session.createQuery("from ClassFor", ClassFor.class);
+            List<ClassFor> classes = query.getResultList();
+            session.getTransaction().commit();
+
+            for (ClassFor classFor : classes) {
+                MenuItem menuItem = new MenuItem(classFor.getName());
+                menuItem.setOnAction(e -> classSelector.setText(classFor.getName()));
+                classSelector.getItems().add(menuItem);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
     }
 
 
